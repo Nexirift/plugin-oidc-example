1638b3a7cd50fa30c4edd00e7e4330919d40f589
Creaous <68230846+Creaous@users.noreply.github.com>
2024-03-05 21:36:29 +1030

    ⚡ Add  useful features from Spark

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,31 @@
-import { createYoga } from 'graphql-yoga';
-import { createServer } from 'node:http';
-import SchemaBuilder from '@pothos/core';
-import { Keycloak } from 'keycloak-backend';
-import { useKeycloak } from '@nexirift/plugin-keycloak';
-import { redisClient } from './redis';
+import { createYoga } from "graphql-yoga";
+import { createServer } from "node:http";
+import SchemaBuilder from "@pothos/core";
+import { Keycloak } from "keycloak-backend";
+import { useKeycloak } from "@nexirift/plugin-keycloak";
+import { redisClient } from "./redis";
 import { Context } from "./context";
+
+require("dotenv").config();
 
 // Create a new instance of Keycloak
 const keycloak = new Keycloak({
-  "realm": "master",
-  "keycloak_base_url": "http://auth.local",
-  "client_id": "plugin-keycloak-test"
-})
+  realm: process.env.AUTH_REALM as string,
+  keycloak_base_url: process.env.AUTH_BASE_URL as string,
+  client_id: process.env.AUTH_CLIENT_ID as string,
+});
 
 // Create a new instance of SchemaBuilder
 const builder = new SchemaBuilder<{
-  Context: Context,
+  Context: Context;
 }>({});
 
 // Define the query type and its fields
 builder.queryType({
   fields: (t) => ({
     hello: t.string({
-      resolve: (_root, _args, ctx: Context) => `Hello ${ctx?.keycloak?.preferred_username || "Anonymous"}`,
+      resolve: (_root, _args, ctx: Context) =>
+        `Hello ${ctx?.keycloak?.preferred_username || "Anonymous"}`,
     }),
   }),
 });
@@ -33,17 +36,17 @@ const yoga = createYoga({
   plugins: [
     useKeycloak({
       keycloak: keycloak,
-      redis: redisClient
-    })
+      redis: redisClient,
+    }),
   ],
 });
 
 // Create a new server using the GraphQL Yoga instance
 const server = createServer(yoga);
 
-// Start the server and listen on port 3000
-server.listen(3000, async () => {
+// Start the server and listen on a port defined by .env
+server.listen(process.env.PORT, async () => {
   await redisClient.connect();
-  console.log("📊 GraphQL Yoga Keycloak Plugin Example")
-  console.log('🚀 Serving at http://localhost:3000/graphql');
+  console.log("📊 GraphQL Yoga Keycloak Plugin Example");
+  console.log(`🚀 Serving at http://localhost:${process.env.PORT}/graphql`);
 });
