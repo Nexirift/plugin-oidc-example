eea37796a0919db0581ce5b15fa258e82ed0277a
Creaous <68230846+Creaous@users.noreply.github.com>
2024-06-28 20:49:04 +0930

    fix: redis

--- src/redis.ts
+++ src/redis.ts
@@ -2,7 +2,9 @@
 import { createClient } from 'redis';
 
 // Create a Redis client using the createClient function
-export const redisClient = createClient();
+export const redisClient = createClient({
+	url: process.env.REDIS_URL as string
+});
 
 // Listen for any errors that occur in the Redis client
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
